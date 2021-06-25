629e44b34b97642af81bc166fbfbbbfb9abe856d
bira1 <bira1999@live.co.uk>
2021-06-25 17:25:12 +0100

    pytorch test

--- pytorchtutpt3.py
+++ pytorchtutpt3.py
@@ -27,11 +27,59 @@ class Net(nn.Module):
         return F.log_softmax(x, dim=1) #dim=1 is similar to axis; which thing is the prob dist which we want to sum to 1; we are distributing amognst outplayer tensor
 
 net = Net()
-print(net)
+#print(net)
 
 X = torch.rand((28,28))
 X = X.view(-1, 28*28) #the -1 specifies that this input will be of an unknown shape; our actual shape is 1 28 28
-print(X)
+#print(X)
 
 output = net(X)
-print(output)
+#print(output)
+
+import torch.optim as optim
+
+optimizer = optim.Adam(net.parameters(), lr=0.001) # this corresponds to everything that are adjustable in the model; lr dictates the size of the step that opt will take to get to local/global minimum loss; usually we decay lr but for this problem simple
+
+EPOCHS = 3
+
+for epoch in range(EPOCHS):
+    for data in trainset: # data is a batch of featuresets and labels
+        X, y = data
+        net.zero_grad()
+        #2 reasons we batch data: 1) reduces training time 2) there is a law of diminishing returns; helps to generalise
+        output = net (X.view(-1, 28*28))
+        loss = F.nll_loss(output, y)
+        loss.backward() #backward propogate
+        optimizer.step()
+    print(loss)
+
+correct = 0
+total = 0
+
+# check how correct model was
+with torch.no_grad():
+    for data in trainset:
+        X, y = data
+        output = net(X.view(-1, 28*28))
+        for idx, i in enumerate(output):
+            if torch.argmax(i) == y[idx]:
+                correct += 1
+            total += 1
+print("Accuracy: ", round(correct/total, 3))
+
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+import matplotlib.pyplot as plt
+plt.imshow(X[1].view(28,28))
+plt.show() #visualisation of input number/what we want to the model to predict
+
+print(torch.argmax(net(X[1].view(-1,28*28))[0])) #output of model
+
+
+
+
+
+
+
+
